332aa353f0dc4b79e4c5c1d1a2e0534cf15e7c95
suryaprakashr1025 <110890454+suryaprakashr1025@users.noreply.github.com>
2022-08-16 09:24:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+//1.json iterate in (for, for in, for Of, forEach)
+
+let student=[{name:'AAAA',age:24,city:"coimbatore",department:"BSC IT"},{name:'BBBB',age:25,city:"chennai",department:"BSC CT"}];
+for (let i=0;i<student.length;i++){
+    console.log(student[i]);
+}
+for (key in student){
+ console.log(key);
+   console.log(student[key]);
+}
+
+for (key of student){
+    console.log(key);
+}
+student.forEach(function(details,index,arr) 
+ {
+    console.log(arr[index]);
+});
+
+
+//2.resume in json format
+
+let resume={
+    name:"surya",
+    age:24,
+    city:"coimbatore",
+    email:"suryaprakashr1025@gmail.com",
+    phoneNo:9566953853,
+    degree:"BSC IT",
+    college:"sri krishna college of arts and science",
+    hobby:"watching movie"
+}
+for (key in resume){
+    console.log(key+":" +resume[key]);
+    
+}
