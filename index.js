db4f8724088425ec9ef8dda133ee0388a80784de
suryaprakashr1025 <110890454+suryaprakashr1025@users.noreply.github.com>
2022-08-16 13:22:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //1.json iterate in (for, for in, for Of, forEach)
 
-let student=[{name:'AAAA',age:24,city:"coimbatore",department:"BSC IT"},{name:'BBBB',age:25,city:"chennai",department:"BSC CT"}];
+let student=[{"name":'AAAA',"age":24,"city":"coimbatore","department":"BSC IT"},{"name":'BBBB',"age":25,"city":"chennai","department":"BSC CT"}];
 for (let i=0;i<student.length;i++){
     console.log(student[i]);
 }
@@ -21,16 +21,17 @@ student.forEach(function(details,index,arr)
 //2.resume in json format
 
 let resume={
-    name:"surya",
-    age:24,
-    city:"coimbatore",
-    email:"suryaprakashr1025@gmail.com",
-    phoneNo:9566953853,
-    degree:"BSC IT",
-    college:"sri krishna college of arts and science",
-    hobby:"watching movie"
+    "name":"surya",
+    "age":24,
+    "city":"coimbatore",
+    "email":"suryaprakashr1025@gmail.com",
+    "phoneNo":9566953853,
+    "degree":"BSC IT",
+    "college":"sri krishna college of arts and science",
+    "hobby":"watching movie"
 }
+
 for (key in resume){
-    console.log(key+":" +resume[key]);
+    console.log(key+":"+resume[key]);
     
 }
